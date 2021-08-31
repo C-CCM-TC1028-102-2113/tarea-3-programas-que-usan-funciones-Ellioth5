c424e7777828ddc7ddea30b5575ab822cfcd8cdb
Ellioth5 <89115163+Ellioth5@users.noreply.github.com>
2021-08-30 22:32:55 -0500

    Update exercise.py

--- assignments/10Bisiesto/src/exercise.py
+++ assignments/10Bisiesto/src/exercise.py
@@ -1,7 +1,18 @@
 
 def main():
     #escribe tu código abajo de esta línea
-    pass
+    def es_bisiesto(x):
+    if x%4==0 and x%400==0:
+        return "True"
+    elif x%100==0:
+        return "False"
+    else:
+        return "False"
+def main():
+    x=int(input("Dame el año: "))
+    y=es_bisiesto(x)
+    print(str(y))
+main()
 
 if __name__=='__main__':
     main()
